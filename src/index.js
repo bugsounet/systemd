213754d63673648f05720d4580f95c6ccfd6293b
Bugsounet - Cédric <github@bugsounet.fr>
2021-05-29 19:16:40 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-const exec = require("shelljs").exec
+
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
 const detailsRegex = /Id=(.+)\nActiveState=(.+)\nSubState=(.+)\nUnitFileState=(.+)\nStateChangeTimestamp=(.+)?/m
 
@@ -9,7 +11,7 @@ class Systemd {
     this.checkService()
   }
 
-  status() {
+  async status() {
     if (!this.init) return { error: "not initialized" }
     const command = [
       `systemctl show ${this.service}`,
@@ -19,41 +21,25 @@ class Systemd {
       'UnitFileState',
       'StateChangeTimestamp'
     ].join(' -p ')
-  
-    const currentStatus = exec(command, { silent: true })
-      .trim()
-      .split('\n\n')
-      .map((serviceData) => {
-        const properties = serviceData.match(detailsRegex)
-        if (!properties) return {
-          name: this.service, 
-          error: "Unknown service"
-        }
-        let [, name, activeState, state, unitFileState] = properties
-        name = sliceLast(name, '.')
-        return {
-          name,
-          state,
-          isActive: activeState === 'active',
-          isDisabled: unitFileState === 'disabled'
-        }
-      })
-    return currentStatus[0]
+
+    const { stdout, stderr } = await exec(command)
+    return GetStdOutResponse(stdout,this.service)
   }
   
-  restart() {
+  async restart() {
     if (!this.init) return { error: "not initialized" }
     const command = `sudo systemctl restart ${this.service}`
-  
-    const currentRestart = exec(command, { silent: true })
-    if (currentRestart.stderr) {
-      let error = sliceLast(currentRestart.stderr, "\n")
+
+    try {
+      const { stdout, stderr } = await exec(command)
+    } catch (e) {
+      let error = sliceLast(e.message, "\n")
       return {
         name: this.service,
         error: error
       }
     }
-    else return {
+    return {
       name: this.service,
       restart: "ok"
     }
@@ -70,6 +56,33 @@ function sliceLast(str, sep = ' ') {
   return splitedStr.length > 1
     ? splitedStr.slice(0, -1).join(sep)
     : str
+}
+
+function GetStdOutResponse(std,service) {
+  return new Promise(resolve => {
+    var values
+    std.trim()
+      .split('\n\n')
+      .map((serviceData) => {
+        const properties = serviceData.match(detailsRegex)
+        if (!properties) {
+          values = {
+            name: service,
+            error: "Unknown service"
+          }
+        } else {
+          let [, name, activeState, state, unitFileState] = properties
+          name = sliceLast(name, '.')
+          values = {
+            name,
+            state,
+            isActive: activeState === 'active',
+            isDisabled: unitFileState === 'disabled'
+          }
+        }
+      })
+    resolve (values)
+  })
 }
 
 module.exports = Systemd
